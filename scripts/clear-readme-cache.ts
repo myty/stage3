8e2bf014f4157fe03e1dffd9f96bf7e62880a26c
Michael Tyson <myty@users.noreply.github.com>
2023-08-20 16:25:49 -0400

    update clear github readme cache

--- scripts/clear-readme-cache.ts
+++ scripts/clear-readme-cache.ts
@@ -1,25 +1,36 @@
+type LineAttributes = { url: string; alt: string };
+
 const response = await fetch("https://github.com/myty/stage3");
 const body = await response.text();
 
-const queue = body.split("<img ").map((line) => {
-  const match = line.match(
-    /^src="(https:\/\/camo\.githubusercontent\.com\/[a-f0-9]+\/[a-f0-9]+)" alt="([^"]+)"/,
-  );
-  if (match) {
-    return { url: match[1], alt: match[2] };
-  }
-}).filter((
-  line,
-): line is { url: RegExpMatchArray[0]; alt: RegExpMatchArray[1] } => !!line);
+const queue = body.split("<img ")
+  .map(lineToAttributes)
+  .filter((line): line is LineAttributes => line != null);
 
-Promise.allSettled(
-  queue.map((obj) => {
-    return fetch(obj.url, { method: "PURGE" }).then(() =>
-      console.log("PURGE", obj.alt)
-    ).catch((error) => {
-      if (error) {
-        console.error("Error: PURGE", obj.alt, error);
+await Promise.allSettled(
+  queue.map(async (obj) => {
+    try {
+      const response = await fetch(obj.url, { method: "PURGE" });
+      const json = await response.json();
+
+      if (json.status !== "ok") {
+        throw new Error(json.status);
       }
-    });
+
+      console.log("PURGE", { request: obj, result: json });
+    } catch (error) {
+      if (error) {
+        console.error("Error: PURGE", { request: obj, result: error });
+      }
+    }
   }),
 );
+
+function lineToAttributes(line: string): LineAttributes | undefined {
+  const [_, url, alt] = line.match(
+    /^src="(https:\/\/camo\.githubusercontent\.com\/[a-f0-9]+\/[a-f0-9]+)" alt="([^"]+)"/,
+  ) ?? [];
+  if (url && alt) {
+    return { url, alt };
+  }
+}
